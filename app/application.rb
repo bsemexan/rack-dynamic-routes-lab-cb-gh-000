a09f7c9f365f9e72980bb5f3e8852bca29ccd79c
Berry  Semexan <bsemexan@gmail.com>
2018-03-03 06:04:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application < Item
 
-@@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
