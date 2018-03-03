75ddce3d125435841a2106d7c00f69f551ef9cf2
Berry  Semexan <bsemexan@gmail.com>
2018-03-03 06:01:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application < Item
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       is_item = @@item.find{|s| s.name == item_name}
-      if @@item.include?(item)
-        resp.write "#{item}"
+      if @@item.include?(is_item)
+        resp.write is_item.price
       else
         resp.status = 400
       end
